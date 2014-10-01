2d0256f8f7c09a745defbd17519b05570d7350e0
Vincius Zilli Pavei <vinicius@paveisistemas.com.br>
2014-09-30 21:49:21 -0300

    Readme

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ ionic-image-lazy-load
 =====================
 
 Directive to Ionic Framework that's only load image when the image is seen by the user 
+
+## Quick Start
+
+Download the file JS ionic-image-lazy-load.js and load into your index.html
+
+``` html
+<script src="lib/ionic-image-lazy-load/ionic-image-lazy-load.js"></script>
+```
+
+Load into your module
+
+``` javascript
+    angular.module('yourapp',
+        ['ionic', 'ionic-image-lazy-load'])
+```
+
+
---
+++ ionic-image-lazy-load.js
@@ -0,0 +1,74 @@
+/**
+ * Created by PAVEI on 30/09/2014.
+ */
+angular.module('ionic-img-lazy-load').directive(
+    'scroller', function ($document, scrollAndResizeListener, $rootScope, $timeout) {
+        return {
+            restrict: 'A',
+            link: function ($scope, $element, $attributes) {
+
+                var id = 0,
+                    scrollTimeoutId,
+                    resizeTimeoutId;
+
+                $scope.invoke = function () {
+                    $rootScope.$broadcast('scrollEvent');
+                }
+
+                $element.bind('scroll', function (e) {
+
+                    $timeout.cancel(scrollTimeoutId);
+
+                    // wait for 200ms and then invoke listeners (simulates stop event)
+                    scrollTimeoutId = $timeout($scope.invoke, 50);
+
+                });
+
+
+            }
+        }
+    });
+
+angular.module('ionic-img-lazy-load').directive(
+    'imageLazySrc', function ($document, scrollAndResizeListener) {
+        return {
+            restrict: 'A',
+            link: function ($scope, $element, $attributes) {
+                var listenerRemover;
+
+
+                var deregistration = $scope.$on('scrollEvent', function () {
+
+                        if (isInView()) {
+                            $element[0].src = $attributes.imageLazySrc; // set src attribute on element (it will load image)
+                            deregistration();
+                        }
+                    }
+                );
+
+                function isInView() {
+
+                    var clientHeight = $document[0].documentElement.clientHeight;
+                    var clientWidth = $document[0].documentElement.clientWidth;
+                    var imageRect = $element[0].getBoundingClientRect();
+                    return  (imageRect.top >= 0 && imageRect.bottom <= clientHeight) && (imageRect.left >= 0 && imageRect.right <= clientWidth);
+                }
+
+                // bind listener
+                listenerRemover = scrollAndResizeListener.bindListener(isInView);
+
+                // unbind event listeners if element was destroyed
+                // it happens when you change view, etc
+                $element.on('$destroy', function () {
+                    deregistration();
+                });
+
+                // explicitly call scroll listener (because, some images are in viewport already and we haven't scrolled yet)
+                if (isInView()) {
+                    $element[0].src = $attributes.imageLazySrc; // set src attribute on element (it will load image)
+                    deregistration();
+                }
+            }
+        }
+    }
+);
