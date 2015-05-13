d8fb1709e827a0710175ee4ad7f21230229ab564
mvidailhet <mvidailhet@mncc.fr>
2015-05-12 18:04:22 -0700

    Possibility to have a ionic spinner while the image load (#1) Added 2 parameters to specify a distance from the bottom and right of the screen where the image will start loading (#5) Possibility to use a background-image instead of an image element.

--- ionic-image-lazy-load.js
+++ ionic-image-lazy-load.js
@@ -2,6 +2,7 @@
  * Created by PAVEI on 30/09/2014.
  * Updated by Ross Martin on 12/05/2014
  * Updated by Davide Pastore on 04/14/2015
+ * Updated by Michel Vidailhet on 05/12/2015
  */
 
 angular.module('ionicLazyLoad', []);
@@ -34,38 +35,71 @@ angular.module('ionicLazyLoad')
         };
 }])
 
-.directive('imageLazySrc', ['$document', '$timeout', '$ionicScrollDelegate',
-    function ($document, $timeout, $ionicScrollDelegate) {
+.directive('imageLazySrc', ['$document', '$timeout', '$ionicScrollDelegate', '$compile',
+    function ($document, $timeout, $ionicScrollDelegate, $compile) {
         return {
             restrict: 'A',
             scope: {
-            	lazyScrollResize: "@lazyScrollResize"
+                lazyScrollResize: "@lazyScrollResize",
+                imageLazyBackgroundImage: "@imageLazyBackgroundImage"
             },
             link: function ($scope, $element, $attributes) {
+                if (!$attributes.imageLazyDistanceFromBottomToLoad) {
+                    $attributes.imageLazyDistanceFromBottomToLoad = 0;
+                }
+                if (!$attributes.imageLazyDistanceFromRightToLoad) {
+                    $attributes.imageLazyDistanceFromRightToLoad = 0;
+                }
+
+                if ($attributes.imageLazyLoader) {
+                    var loader = $compile('<div class="image-loader-container"><ion-spinner class="image-loader" icon="' + $attributes.imageLazyLoader + '"></ion-spinner></div>')($scope);
+                    $element.after(loader);
+                }
 
                 var deregistration = $scope.$on('lazyScrollEvent', function () {
                         //console.log('scroll');
                         if (isInView()) {
-                            
-                            //Bind "load" event
-                            $element.bind("load", function(e){
-                        		if($scope.lazyScrollResize == "true"){
-                        			//Call the resize to recalculate the size of the screen
-                        			$ionicScrollDelegate.resize();
-                        		}
-                        	});
-                            
-                            $element[0].src = $attributes.imageLazySrc; // set src attribute on element (it will load image)
+                            loadImage();
                             deregistration();
                         }
                     }
                 );
 
+                function loadImage() {
+                    //Bind "load" event
+                    $element.bind("load", function (e) {
+                        if ($attributes.imageLazyLoader) {
+                            loader.remove();
+                        }
+                        $element[0].src = $attributes.imageLazySrc; // set src attribute on element (it will load image)
+                        if ($scope.lazyScrollResize == "true") {
+                            //Call the resize to recalculate the size of the screen
+                            $ionicScrollDelegate.resize();
+                        }
+                    });
+
+                    if ($scope.imageLazyBackgroundImage == "true") {
+                        var bgImg = new Image();
+                        bgImg.onload = function () {
+                            if ($attributes.imageLazyLoader) {
+                                loader.remove();
+                            }
+                            $element[0].style.backgroundImage = 'url(' + $attributes.imageLazySrc + ')'; // set style attribute on element (it will load image)
+                            if ($scope.lazyScrollResize == "true") {
+                                //Call the resize to recalculate the size of the screen
+                                $ionicScrollDelegate.resize();
+                            }
+                        };
+                        bgImg.src = $attributes.imageLazySrc;
+                    }
+                }
+
                 function isInView() {
                     var clientHeight = $document[0].documentElement.clientHeight;
                     var clientWidth = $document[0].documentElement.clientWidth;
                     var imageRect = $element[0].getBoundingClientRect();
-                    return  (imageRect.top >= 0 && imageRect.bottom <= clientHeight) && (imageRect.left >= 0 && imageRect.right <= clientWidth);
+                    return (imageRect.top >= 0 && imageRect.top <= clientHeight + parseInt($attributes.imageLazyDistanceFromBottomToLoad))
+                        && (imageRect.left >= 0 && imageRect.left <= clientWidth + parseInt($attributes.imageLazyDistanceFromRightToLoad));
                 }
 
                 // bind listener
@@ -78,12 +112,12 @@ angular.module('ionicLazyLoad')
                 });
 
                 // explicitly call scroll listener (because, some images are in viewport already and we haven't scrolled yet)
-                $timeout(function() {
+                $timeout(function () {
                     if (isInView()) {
-                        $element[0].src = $attributes.imageLazySrc; // set src attribute on element (it will load image)
+                        loadImage();
                         deregistration();
                     }
                 }, 500);
             }
         };
-}]);
+    }]);
